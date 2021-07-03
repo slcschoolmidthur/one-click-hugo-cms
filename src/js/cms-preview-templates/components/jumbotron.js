02bef1ff9ff8a208852f7429a36875b4c87db615
slcschoolmidthur <86719949+slcschoolmidthur@users.noreply.github.com>
2021-07-04 01:31:38 +0530

    Update jumbotron.js

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -14,9 +14,9 @@ export default class Jumbotron extends React.Component {
                 { title }
               </h1>
             </div>
-            <div className="mw7 relative bg-fix-primary">
+            
               {subtitle && <p className="b f4 di lh-title mb3 white mw6" bg-primary>{ subtitle }</p>}
-            </div>
+           
           </div>
         </div>
       </div>
